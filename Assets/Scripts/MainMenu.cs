10ee7d842de5e0c4950bd8e1156794d927f18720
William Jeon <iamwilly99@gmail.com>
2022-06-24 20:26:14 -0700

    hope it works

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     void StartGame()
     {
-
+        
     }
     void QuitGame()
     {
