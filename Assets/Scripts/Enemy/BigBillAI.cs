7d5d7d064beb473cba9d46d5d93cfdaf6be333b1
William Jeon <iamwilly99@gmail.com>
2022-06-26 18:54:29 -0700

    final boosss

---
+++ Assets/Scripts/Enemy/BigBillAI.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBillAI : MonoBehaviour
+{
+    private Transform player;
+    public Transform cbow;
+
+    public float horRotSpeed;      // how fast enemies rotate towards the player horizontally
+    public float verRotSpeed;      // how fast the crossbow rotates towards the player vertically
+    public bool dead;
+
+    public float fireFrequency = 1f;
+    private float offcd;
+    public float maxRange = 30f;
+    [SerializeField] Transform proj;
+    [SerializeField] Transform rangedSpawn;
+
+    public AudioSource tense;
+    public AudioSource fire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offcd = Time.time + fireFrequency;
+        dead = false;
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (!dead)
+        {
+            // rotates enemy and crossbow towards player
+            var newRotation1 = Quaternion.LookRotation(player.position - transform.position);
+            newRotation1.x = 0;
+            newRotation1.z = 0;
+
+            var newRotation2 = Quaternion.LookRotation(player.position - cbow.position);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation1, Time.deltaTime * horRotSpeed);
+            cbow.rotation = Quaternion.Slerp(cbow.rotation, newRotation2, Time.deltaTime * verRotSpeed);
+
+            // fires arrow
+            if (offcd < Time.time && Vector3.Distance(transform.position, player.position) < maxRange)
+            {
+                Invoke(nameof(FireArrow), Time.deltaTime * 1f);
+                offcd = Time.time + fireFrequency;
+            }
+        }
+    }
+
+    private void FireArrow()
+    {
+        Instantiate(proj, rangedSpawn.position, rangedSpawn.rotation);
+        fire.Play();
+    }
+    private void FireArrow2()
+    {
+        Instantiate(proj, rangedSpawn.position, rangedSpawn.rotation);
+        fire.Play();
+    }
+}
