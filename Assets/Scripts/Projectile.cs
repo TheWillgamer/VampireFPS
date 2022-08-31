fa751eb5adf695b226109c761e45e4fd744ed1db
William Jeon <iamwilly99@gmail.com>
2022-08-31 06:35:23 -0700

    fixed projectiles going through walls

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,20 +7,40 @@ public class Projectile : MonoBehaviour
     [SerializeField] float speed = 30.0f;
     [SerializeField] float aliveTime = 5.0f;
     float deathTime;
+    private float _colliderRadius;
 
     // Start is called before the first frame update
     void Start()
     {
         deathTime = Time.time + aliveTime;
+        SphereCollider sc = GetComponent<SphereCollider>();
+        _colliderRadius = sc.radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Time.deltaTime * speed;
         if (Time.time > deathTime)
         {
             Destroy(transform.parent.gameObject);
         }
+
+        //Determine how far object should travel this frame.
+        float travelDistance = (speed * Time.deltaTime);
+        //Set trace distance to be travel distance + collider radius.
+        float traceDistance = travelDistance + _colliderRadius;
+
+        //Explode bullet if it goes through the wall
+        int layerMask = 1 << 6;
+
+        RaycastHit hit;
+        // Does the ray intersect any walls
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, traceDistance, layerMask))
+        {
+            Destroy(transform.parent.gameObject);
+        }
+
+        transform.position += transform.forward * travelDistance;
     }
 }
