5fc1112df396e45ba58b7c52eddf74be0a7bc068
RaccoonTrashEater <firewolf5825@gmail.com>
2022-09-01 19:12:56 -1000

    settings menu functionality complete

--- Assets/StartMenu.cs
+++ Assets/StartMenu.cs
@@ -7,7 +7,7 @@ public class StartMenu : MonoBehaviour
 {
     public void PlayGame ()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
 
     }
 
