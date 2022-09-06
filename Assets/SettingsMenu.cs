6ae93cd736cc93a5d6cc757f102a27134e01fda2
RaccoonTrashEater <firewolf5825@gmail.com>
2022-09-06 12:15:17 -1000

    Sensitivity bar soon to be applied on player character

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -12,8 +12,19 @@ public class SettingsMenu : MonoBehaviour
 
     Resolution[] resolutions;
 
+    public bool initialized = false;
+
+    public Slider mouseSensitivitySlider;
+
     void Start ()
     {
+        if (PlayerPrefs.HasKey("Sensitivity"))
+        {
+            mouseSensitivitySlider.value = PlayerPrefs.GetFloat ("Sensitivity");
+            Debug.Log("Loaded a sensitivity of " + mouseSensitivitySlider.value);
+        }
+        initialized = true;
+
         resolutions = Screen.resolutions;
 
         resolutionDropdown.ClearOptions();
@@ -36,6 +47,16 @@ public class SettingsMenu : MonoBehaviour
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetMouseSensitivity(float val)
+    {
+        if (! initialized) return;
+        if (! Application.isPlaying) return;
+
+
+        PlayerPrefs.SetFloat("Sensitivity", val);
+        Debug.Log("Set sensitivity to " + val);
     }
 
     public void SetResolution (int resolutionIndex)
