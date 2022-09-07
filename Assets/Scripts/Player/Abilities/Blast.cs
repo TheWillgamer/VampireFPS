6264b555cf90dbe8941404d7129e39074e6d0ca1
William Jeon <iamwilly99@gmail.com>
2022-09-07 00:11:20 -0700

    blast damage implemented

--- Assets/Scripts/Player/Abilities/Blast.cs
+++ Assets/Scripts/Player/Abilities/Blast.cs
@@ -6,6 +6,17 @@ public class Blast : Ability
 {
     public float dashSpeed = 2000f;
     public float activateTime = 0.1f;
+
+    [SerializeField] Transform blastPoint;
+    [SerializeField] private float radius;
+    private float reach;
+
+    public int minDamage = 5;
+    public int maxDamage = 5;
+
+    public float minKnockback = 10000f;
+    public float maxKnockback = 10000f;
+
     public AudioSource sound;
 
     public override void UseAbility()
@@ -18,5 +29,20 @@ public class Blast : Ability
     void Activate()
     {
         rb.AddForce(-pm.playerCam.transform.forward * dashSpeed);
+        reach = (blastPoint.position - transform.position).magnitude;
+
+        // blasts enemies away
+        foreach (Collider col in Physics.OverlapCapsule(transform.position, blastPoint.position, radius))
+        {
+            if (col.tag == "Enemy")
+            {
+                Vector3 direction = col.transform.position - transform.position;
+                float distance = direction.magnitude;
+
+                EnemyHitDetection ehd = col.gameObject.GetComponent<EnemyHitDetection>();
+                ehd.TakeDamage(maxDamage - (int)((distance / reach) * (maxDamage - minDamage)));
+                ehd.Knockback(maxKnockback - (distance / reach) * (maxKnockback - minKnockback), direction.normalized);
+            }
+        }
     }
 }
