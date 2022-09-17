cdf1ae4ea7ae477a587a957d703992c0f30a7683
William Jeon <iamwilly99@gmail.com>
2022-09-16 21:24:05 -0700

    Rigidbody wall check

---
+++ Assets/Scripts/RigidBodyWallCheck.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyWallCheck : MonoBehaviour
+{
+    public Transform playerCam;
+    private Rigidbody rb;
+    private float _colliderRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CapsuleCollider cc = GetComponent<CapsuleCollider>();
+        _colliderRadius = cc.radius;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = rb.velocity;
+        //Determine how far object should travel this frame.
+        float travelDistance = (velocity.magnitude * Time.deltaTime);
+        //Set trace distance to be travel distance + collider radius.
+        float traceDistance = travelDistance + _colliderRadius;
+
+        //Explode bullet if it goes through the wall
+        RaycastHit hit;
+        // Does the ray intersect any walls
+
+        if (Physics.SphereCast(playerCam.position, _colliderRadius, velocity, out hit, traceDistance))
+        {
+            rb.velocity = rb.velocity / 5;
+        }
+    }
+}
