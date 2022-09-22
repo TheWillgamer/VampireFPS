8c2cfa24e6519a799fb7b78d0d1cb670f6b1dc7a
William Jeon <iamwilly99@gmail.com>
2022-09-21 17:57:51 -0700

    blast prefab

--- Assets/Scripts/Player/Abilities/Blast.cs
+++ Assets/Scripts/Player/Abilities/Blast.cs
@@ -8,9 +8,10 @@ public class Blast : Ability
     public float dashSpeed = 2000f;
     public float activateTime = 0.1f;
 
-    [SerializeField] Transform blastPoint;
+    [SerializeField] Transform blastEffect;
+    [SerializeField] Transform blastPoint;      // where the blast effect spawns
     [SerializeField] private float radius;
-    private float reach;
+    [SerializeField] private float range;
 
     public int minDamage = 5;
     public int maxDamage = 5;
@@ -29,21 +30,26 @@ public class Blast : Ability
 
     void Activate()
     {
+        Instantiate(blastEffect, blastPoint.position, blastPoint.rotation);
+
         rb.velocity = rb.velocity * movementReductionMultiplier;
         rb.AddForce(-pm.playerCam.transform.forward * dashSpeed);
-        reach = (blastPoint.position - transform.position).magnitude;
 
-        // blasts enemies away
-        foreach (Collider col in Physics.OverlapCapsule(transform.position, blastPoint.position, radius))
+        RaycastHit[] hits;
+        hits = Physics.SphereCastAll(pm.playerCam.transform.position, radius, pm.playerCam.transform.forward, range);
+
+        for (int i = 0; i < hits.Length; i++)
         {
+            Collider col = hits[i].collider;
+
             if (col.tag == "Enemy")
             {
                 Vector3 direction = col.transform.position - transform.position;
                 float distance = direction.magnitude;
 
                 EnemyHitDetection ehd = col.gameObject.GetComponent<EnemyHitDetection>();
-                ehd.TakeDamage(maxDamage - (int)((distance / reach) * (maxDamage - minDamage)));
-                ehd.Knockback(maxKnockback - (distance / reach) * (maxKnockback - minKnockback), direction.normalized);
+                ehd.TakeDamage(maxDamage - (int)((distance / range) * (maxDamage - minDamage)));
+                ehd.Knockback(maxKnockback - (distance / range) * (maxKnockback - minKnockback), direction.normalized);
             }
         }
     }
