76fa8f9ff0ac6146ab549de81ba5582df69ae0b2
William Jeon <iamwilly99@gmail.com>
2022-09-27 18:54:30 -0700

    sniper wip

---
+++ Assets/Scripts/Enemy/SniperAI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperAI : MonoBehaviour
+{
+    [SerializeField] private float turnSpeed;
+    [SerializeField] private float alertRadius;     // how fast enemy turns towards the player
+    private Transform player;
+    private bool active;
+    LineRenderer lr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        active = true;
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relLoc = player.position - transform.position;
+
+        if (active && relLoc.magnitude < alertRadius)
+        {
+            relLoc.y = 0;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(relLoc, Vector3.up), turnSpeed * Time.deltaTime);
+        }
+    }
+}
