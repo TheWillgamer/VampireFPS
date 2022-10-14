c28a4a38e8c3631520defeab9d9250d801bf7950
William Jeon <iamwilly99@gmail.com>
2022-10-13 21:02:35 -0700

    sniper stuff

--- Assets/Scripts/Enemy/SniperAI.cs
+++ Assets/Scripts/Enemy/SniperAI.cs
@@ -7,12 +7,18 @@ public class SniperAI : EnemyAI
     [SerializeField] private float alertRadius;
     [SerializeField] private float attackRange;
     [SerializeField] private float attackChargeTime;
+    [SerializeField] private float attackCooldownTime;
     [SerializeField] private Transform laserStart;
+    [SerializeField] private Transform proj;
+    [SerializeField] private Transform rangedSpawn;
     private Transform player;
     private bool active;
+    private bool coolingDown;
     private float charge;
     LineRenderer lr;
     Vector3[] points;
+
+    public AudioSource fire;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +28,8 @@ public class SniperAI : EnemyAI
         lr = GetComponent<LineRenderer>();
         points = new Vector3[2];
         charge = 0;
+        coolingDown = false;
+        lr.endWidth = 0;
     }
 
     // Update is called once per frame
@@ -29,7 +37,7 @@ public class SniperAI : EnemyAI
     {
         Vector3 relLoc = player.position - transform.position;
 
-        if (active && relLoc.magnitude < alertRadius)
+        if (active && !coolingDown && relLoc.magnitude < alertRadius)
         {
             transform.rotation = Quaternion.LookRotation(player.transform.position - transform.position, Vector3.up);
 
@@ -40,10 +48,10 @@ public class SniperAI : EnemyAI
                 if (hit.collider.tag == "Player")
                 {
                     charge += Time.deltaTime;
-                    if (charge > attackChargeTime)
-                        Fire();
                     lr.endWidth = 1f - charge / attackChargeTime;
                     points[1] = player.transform.position - transform.forward * .4f;
+                    if (charge > attackChargeTime)
+                        Fire();
                 }
                 else
                 {
@@ -60,9 +68,22 @@ public class SniperAI : EnemyAI
                 lr.SetPositions(points);
             }
         }
+        else if (coolingDown)
+        {
+            charge += Time.deltaTime;
+            if (charge > attackCooldownTime)
+            {
+                charge = 0;
+                coolingDown = false;
+            }
+        }
     }
     void Fire()
     {
         charge = 0;
+        Instantiate(proj, rangedSpawn.position, rangedSpawn.rotation);
+        fire.Play();
+        coolingDown = true;
+        lr.endWidth = 0;
     }
 }
