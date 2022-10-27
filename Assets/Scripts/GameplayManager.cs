605713bd6b569bb6da2e880b5d0c8aab2df6ccd2
William Jeon <iamwilly99@gmail.com>
2022-10-27 00:09:40 -0700

    stuff

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class GameplayManager : MonoBehaviour
 {
-    private Time startTime = null;
+    private float startTime = -1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startTime = Time.time;
+        Debug.Log(startTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(startTime >= 0)
+        {
+            Debug.Log(Time.time - startTime);
+        }
     }
 
     void DisplayTime()
