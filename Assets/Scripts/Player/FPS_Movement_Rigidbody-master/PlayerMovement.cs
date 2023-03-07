626177a335005e6008dacfd711b33b1ed1ccf8b7
William Jeon <iamwilly99@gmail.com>
2023-03-06 21:55:53 -0800

    oops

--- Assets/Scripts/Player/FPS_Movement_Rigidbody-master/PlayerMovement.cs
+++ Assets/Scripts/Player/FPS_Movement_Rigidbody-master/PlayerMovement.cs
@@ -100,7 +100,7 @@ public class PlayerMovement : MonoBehaviour
         onMovingPlatform = false;
         canGroundJump = false;
         hLand = false;
-        paused = true;
+        //paused = true;
 
         defaultFOV = camera.fieldOfView;
     }
