d2cee2712a5c5b57b3d377ce0afb3882d3f16faf
William Jeon <iamwilly99@gmail.com>
2023-03-08 00:52:50 -0800

    difficulty stuff

--- Assets/Scripts/DifficultyChooser.cs
+++ Assets/Scripts/DifficultyChooser.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class DifficultyChooser : MonoBehaviour
 {
+    public int difficulty;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (PlayerPrefs.HasKey("Difficulty"))
+        {
+            difficulty = PlayerPrefs.GetInt("Difficulty"); ;
+        }
+        else
+            PlayerPrefs.SetInt("Difficulty", 1);
     }
 }
