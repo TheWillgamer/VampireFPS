80866209b673ccb7c16d23889ff9a351d4ce532d
William Jeon <iamwilly99@gmail.com>
2023-03-30 22:57:17 -0700

    color lighting system

--- Assets/Scripts/SkyboxTransfer.cs
+++ Assets/Scripts/SkyboxTransfer.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class SkyboxTransfer : MonoBehaviour
 {
     [SerializeField] private Material otherSkybox;
+    [SerializeField] private Color otherSkyboxColor;
 
     // Start is called before the first frame update
     void Start()
     {
         RenderSettings.skybox = otherSkybox;
+        RenderSettings.ambientLight = otherSkyboxColor;
     }
 }
