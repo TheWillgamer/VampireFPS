e025d9b466074912aa30b7a0cfccdc4f63cf71fb
William Jeon <iamwilly99@gmail.com>
2023-06-01 01:02:55 -0700

    nothing

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -7,23 +7,21 @@ using UnityEngine;
 public struct Level
 {
     public string name;
-    public bool completed;
     public float fastestTime;
-    public int enemiesKilled;
-    public int secretsFound;
+    public int highestScore;
+    public string secretsFound;
 
-    public Level(string n, bool c, float t, int e, int s)
+    public Level(string n, float t, int h, string s)
     {
         name = n;
-        completed = c;
         fastestTime = t;
-        enemiesKilled = e;
+        highestScore = h;
         secretsFound = s;
     }
 
     public override string ToString()
     {
-        return name + "," + completed.ToString() + "," + fastestTime.ToString() + "," + enemiesKilled.ToString() + "," + secretsFound.ToString();
+        return name + "," + fastestTime.ToString() + "," + highestScore.ToString() + "," + secretsFound;
     }
 }
 
@@ -46,12 +44,17 @@ public class SaveData : MonoBehaviour
             playerData = new PlayerData();
             playerData.levelsCompleted = 0;
             playerData.timePlayed = 0f;
-            playerData.totalEnemiesKilled = 0;
             playerData.totalSecretsFound = 0;
             playerData.levels = new Level[]
             {
-                new Level ("Level0", false, 0, 0, 0),
-                new Level ("Hellfall", false, 0, 0, 0)
+                new Level ("Level1", 0, 0, "000"),
+                new Level ("Level2", 0, 0, "00"),
+                new Level ("Level3", 0, 0, "00"),
+                new Level ("Level4", 0, 0, "00"),
+                new Level ("Level5", 0, 0, "00"),
+                new Level ("Level6", 0, 0, "00"),
+                new Level ("Level7", 0, 0, "00"),
+                new Level ("Level8", 0, 0, "00")
             };
 
             Save(playerData);
@@ -78,14 +81,13 @@ public class SaveData : MonoBehaviour
         string[] dataChunks = data.Split("\n");
         pd.levelsCompleted = Convert.ToInt32(dataChunks[0].Split(":")[1]);
         pd.timePlayed = float.Parse(dataChunks[1].Split(":")[1]);
-        pd.totalEnemiesKilled = Convert.ToInt32(dataChunks[2].Split(":")[1]);
-        pd.totalSecretsFound = Convert.ToInt32(dataChunks[3].Split(":")[1]);
+        pd.totalSecretsFound = Convert.ToInt32(dataChunks[2].Split(":")[1]);
         pd.levels = new Level[dataChunks.Length - 7];
 
         for (int i = 6; i < dataChunks.Length - 1; i++)
         {
             string[] chunks = dataChunks[i].Split(",");
-            pd.levels[i - 6] = new Level(chunks[0], chunks[1]=="1", float.Parse(chunks[2]), Convert.ToInt32(chunks[3]), Convert.ToInt32(chunks[4]));
+            //pd.levels[i - 6] = new Level(chunks[0], chunks[1]=="1", float.Parse(chunks[2]), Convert.ToInt32(chunks[3]), Convert.ToInt32(chunks[4]));
         }
 
         return pd;
@@ -97,7 +99,6 @@ public class PlayerData
 {
     public int levelsCompleted;
     public float timePlayed;
-    public int totalEnemiesKilled;
     public int totalSecretsFound;
     public Level[] levels;
 
@@ -105,7 +106,6 @@ public class PlayerData
     {
         string toReturn = "LevelsCompleted:" + levelsCompleted.ToString() + "\n"
             + "TimeSpentInLevels:" + timePlayed.ToString() + "\n"
-            + "EnemiesKilled:" + totalEnemiesKilled.ToString() + "\n"
             + "SecretsFound:" + totalSecretsFound.ToString() + "\n\n";
 
         toReturn += "LevelName,LevelCompletion,FastestTime,EnemiesKilled,SecretsFound\n";
