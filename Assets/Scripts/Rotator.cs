bf27b561ecbb4bbedd1183bb12099a19cdc1501e
RaccoonTrashEater <firewolf5825@gmail.com>
2023-07-21 13:34:01 -1000

    Updates baby

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private Vector3 _rotation;
+    [SerializeField] private float _speed;
+
+    void Update()
+    {
+        transform.Rotate(_rotation * _speed * Time.deltaTime);
+    }
+
+
+}
